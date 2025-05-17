666922651598e097c95a3c57880bfc0ea03fe175
ringtaa <eeeiqjjj50@gmail.com>
2025-05-17 00:04:17 -0400

    Update faraway.lua

--- faraway.lua
+++ faraway.lua
@@ -6,7 +6,7 @@ end
 
 local character = player.Character or player.CharacterAdded:Wait()
 if character and character:FindFirstChild("HumanoidRootPart") then
-    character.HumanoidRootPart.CFrame = CFrame.new(Vector3.new(-306.47, 3, -48748.65))
+    character.HumanoidRootPart.CFrame = CFrame.new(Vector3.new(-306.47, 4, -48748.65))
 else
     warn("HumanoidRootPart not found!")
 end
